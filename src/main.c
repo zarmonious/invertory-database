ec77f51ffe23cc769fb31a448f50b9bd5bfb8f47
Lolita <43146446+zarmonious@users.noreply.github.com>
2020-07-11 20:46:45 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -38,17 +38,17 @@ int main(int argc, char **argv)
     do
     {
         printf("Menue:\n");
-        printf("1 - Alle Datensaetze ausgeben\n");
-        printf("2 - Eintrag hinzufuegen\n");
-        printf("3 - Eintrag loeschen\n");
-        printf("4 - Datei speichern\n");
-        printf("5 - Datei laden\n");
-        printf("6 - In Datei suchen\n");
-        printf("7 - Datensaetze sortieren\n");
-        printf("8 - Eintraege aus Datei hinzufuegen\n");
+        printf("1 - Show all entries\n");
+        printf("2 - Add a new entry\n");
+        printf("3 - Delete an entry\n");
+        printf("4 - Save in a file\n");
+        printf("5 - Load a file\n");
+        printf("6 - Search in a file\n");
+        printf("7 - Sort entries\n");
+        printf("8 - Add entries from file\n");
         printf("0 - Programm beenden\n");
         printf("\n");
-        wahl = readValue("Deine Wahl");
+        wahl = readValue("Your choice");
         printf("\n");
 
         switch (wahl)
@@ -72,22 +72,22 @@ int main(int argc, char **argv)
         case 5:
             if ( loadDB(items, DATABASE_SIZE, "outputFile.txt") == 0 )
             {
-                printf("Datenbank wurde erfolgreich aus Datei geladen.\n");
+                printf("Database was loaded successfully from file.\n");
             }
             break;
 
         case 6:
-            readString("Suchwort", key, TEXT_LEN);
+            readString("Searchword", key, TEXT_LEN);
             searchFile("outputFile.txt", key, caseIns);
             break;
 
         case 7:
-            whichWahl = readValue("Nach was sortiren (1 - Inventar-Nr., 2 - Anschaffungsjahr)");
+            whichWahl = readValue("Sort by what (1 - Inventory-Nr., 2 - Acquisition-year)");
             sortItems(items, DATABASE_SIZE, whichWahl);
             break;
 
         case 8:
-            readString("Suchwort", key, TEXT_LEN);
+            readString("Searchword", key, TEXT_LEN);
             addFromFile(items, DATABASE_SIZE, "outputFile.txt", key);
             break;
 
@@ -95,7 +95,7 @@ int main(int argc, char **argv)
             return 0;
 
         default:
-            fprintf( stderr, "Fehler. Ungueltige Eingabe. Noch ein mal probieren.\n");
+            fprintf( stderr, "Error. Invalid Input. Try again.\n");
         }
 
         printf("\n");
